64773c07036802b1e6be8ac825c24dd58cd9e312
muschellij2 <muschellij2@gmail.com>
2017-10-05 00:38:52 -0400

    Fixing authors.

--- R/prec2dens.R
+++ R/prec2dens.R
@@ -1,3 +1,18 @@
+#' Calculate Dens value from Precision Matrix
+#'
+#' This function evaluates the density level of a precision matrix based on the
+#' Dens criterion function in equation (5) of Wang et al. (2016).
+#'
+#'
+#' @param precision Input precision matrix (symmetric and positive definite).
+#' @return Density level from Precision matrix.
+#' @author Yikai Wang, Jian Kang, Phebe Brenne Kemmer and Ying Guo\cr
+#' Maintainer: Yikai Wang \email{yikai.wang@@emory.edu}
+#' @references Wang, Y., Kang, J., Kemmer P. and Guo, Y. (2016).  \emph{ An
+#' efficient and reliable statistical method for estimating functional
+#' connectivity in large scale brain networks using partial correlation.  }
+#' Front. Neurosci. 10:123. doi: 10.3389/fnins.2016.00123
+#' @export
 prec2dens<- function(precision)
 {
   return(sum(abs(precision)))
